9aa752d79078d27f3a6aec48d9ec7a2b46abc353
Rachel Whitmore <rachel.whitmore@mandg.com>
2021-04-19 12:18:22 +0100

    refactor #2

--- TicTacToeKata/TicTacToeKata.Source/TicTacToe.cs
+++ TicTacToeKata/TicTacToeKata.Source/TicTacToe.cs
@@ -13,19 +13,8 @@ namespace TicTacToeKata.Source
         bool _secondPlayerTurn = false;
         private string _player = "FirstPlayer";
         private readonly Dictionary<int, char> _positionToLetter = new Dictionary<int, char>();
+        private readonly WinningPositionsWrapper _winningPositionsWrapper = new WinningPositionsWrapper();
 
-        readonly List<int[]> _listOfWinningPositions = new List<int[]>
-        {
-            new [] {1,2,3},
-            new [] {4,5,6},
-            new [] {7,8,9},
-            new [] {1,4,7},
-            new [] {2,5,8},
-            new [] {3,6,9},
-            new [] {1,5,9},
-            new [] {3,5,7}
-
-        };
         public char GetCurrentPlayer()
         {
             PlayerToLetterWrapper playerToLetterWrapper = new PlayerToLetterWrapper(_player);
@@ -65,7 +54,7 @@ namespace TicTacToeKata.Source
 
         public string GetWinner()
         {
-            foreach (var winningPosition in _listOfWinningPositions)
+            foreach (var winningPosition in _winningPositionsWrapper.listOfWinningPositions)
             {
                 var firstWinningPosition = winningPosition[0];
                 var secondWinningPosition = winningPosition[1];
@@ -87,98 +76,7 @@ namespace TicTacToeKata.Source
 
             return "DRAW";
         }
-        /**
-        readonly Dictionary<string, char> _playerToLetter = new Dictionary<string, char>
-        {
-            {"FirstPlayer", 'X'},
-            {"SecondPlayer", 'O'}
-        };
 
-        
-        
-        bool _firstPlayerTurn = true;
-        bool _secondPlayerTurn = false;
-        private string _player = "FirstPlayer";
-
-        readonly List<int[]> _listOfWinningPositions = new List<int[]>
-        {
-            new [] {1,2,3},
-            new [] {4,5,6},
-            new [] {7,8,9},
-            new [] {1,4,7},
-            new [] {2,5,8},
-            new [] {3,6,9},
-            new [] {1,5,9},
-            new [] {3,5,7}
-
-        };
-
-        public char GetCurrentPlayer()
-        {
-            return _playerToLetter[_player];
-        }
-
-    
-
-        public string MarkAtPositions(int[] position)
-        {
-            if (position.Length > 9)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            foreach (var pos in position)
-            {
-                if (MarkAtPosition(pos, out var winner)) 
-                    return winner;
-            }
-
-            return "DRAW";
-
-        }
-
-        private bool MarkAtPosition(int pos, out string s)
-        {
-            var noughtOrCross = GetCurrentPlayer();
-            try
-            {
-                _positionToLetter.Add(pos, noughtOrCross);
-                if (GetWinner(noughtOrCross))
-                {
-                    {
-                        s = $"{noughtOrCross} wins!";
-                        return true;
-                    }
-                }
-
-                SwapPlayers();
-            }
-            catch (ArgumentException)
-            {
-                throw new CanNotPlayPositionAlreadyPlayed();
-            }
-
-            return false;
-        }
-
-        private void SwapPlayers()
-        {
-            if (_firstPlayerTurn)
-            {
-                _firstPlayerTurn = false;
-                _secondPlayerTurn = true;
-                _player = "FirstPlayer";
-            }
-            if (_secondPlayerTurn)
-            {
-                _secondPlayerTurn = false;
-                _firstPlayerTurn = true;
-                _player = "SecondPlayer";
-            }
-        }
-
-        
-        **/
     }
 }  
 
---
+++ TicTacToeKata/TicTacToeKata.Source/WinningPositionsWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToeKata.Source
+{
+    public class WinningPositionsWrapper
+    {
+        public readonly List<int[]> listOfWinningPositions = new List<int[]>
+        {
+            new [] {1,2,3},
+            new [] {4,5,6},
+            new [] {7,8,9},
+            new [] {1,4,7},
+            new [] {2,5,8},
+            new [] {3,6,9},
+            new [] {1,5,9},
+            new [] {3,5,7}
+
+        };
+    }
+}
